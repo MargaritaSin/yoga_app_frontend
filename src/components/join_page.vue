d276d34b6c0057309e9472b5dc05523431719a3b
MargaritaSin <fanfikov@gmail.com>
2024-04-30 22:17:18 +0300

    Элемент юзера полностью реализован

--- src/components/join_page.vue
+++ src/components/join_page.vue
@@ -15,11 +15,17 @@
                 <button @click.prevent="login()">Вход</button>
             </form>
             <form v-else>
-                <input type="text" v-model="user.firstName" placeholder="Имя">
-                <input type="text" v-model="user.lastName" placeholder="Фамилия">
-                <input type="tel" v-model="user.phoneNumber" placeholder="Номер телефона">
-                <input type="date" v-model="user.birthDate" placeholder="Дата рождения">
+                <input type="text" v-model="user.client_first_name" placeholder="Имя">
+                <input type="text" v-model="user.client_last_name" placeholder="Фамилия">
+                <input type="text" v-model="user.client_middle_name" placeholder="Отчество">
+                <input type="tel" v-model="user.client_phone_number" placeholder="Номер телефона">
+                <input type="date" v-model="user.client_birth_date" placeholder="Дата рождения">
+                <input type="password" v-model="user.client_password_hash" placeholder="Пароль">
+                <input type="password" v-model="confirmPassword" placeholder="Повторите пароль">
                 <button @click.prevent="registerUser()">Зарегистрироваться</button>
+                <div v-if="errorMessages.length">
+                    <p v-for="error in errorMessages" :key="error">{{ error }}</p>
+                </div>
             </form>
         </div>
     </div>
@@ -33,11 +39,15 @@ export default {
             email: "",
             emailChecked: false,
             emailExists: false,
+            confirmPassword: "",
+            errorMessages: [],
             user: {
-                firstName: "",
-                lastName: "",
-                phoneNumber: "",
-                birthDate: ""
+                client_first_name: "",
+                client_last_name: "",
+                client_middle_name: "",
+                client_birth_date: "",
+                client_phone_number: "",
+                client_password_hash: ""
             }
         };
     },
@@ -57,33 +67,77 @@ export default {
             }
         },
         async registerUser() {
+            this.errorMessages = [];
+            // Проверка на заполненность всех обязательных полей
+            const requiredFields = {
+                'Имя': this.user.client_first_name,
+                'Фамилия': this.user.client_last_name,
+                'Отчество': this.user.client_middle_name,
+                'Дата рождения': this.user.client_birth_date,
+                'Номер телефона': this.user.client_phone_number,
+                'Пароль': this.user.client_password_hash
+            };
+
+            for (const [key, value] of Object.entries(requiredFields)) {
+                if (!value) this.errorMessages.push(`Поле "${key}" не должно быть пустым.`);
+            }
+
+            // Проверка совпадения паролей
+            if (this.user.client_password_hash !== this.confirmPassword) {
+                this.errorMessages.push("Пароли не совпадают.");
+            }
+
+            // Если есть ошибки, не отправляем форму
+            if (this.errorMessages.length > 0) {
+                return;
+            }
+
             try {
-                let response = await fetch('http://localhost:8080/api/user/register',
+                // Добавляем email в данные для отправки
+                let userData = {
+                    ...this.user,
+                    client_email: this.email
+                };
                 
-                 {
+                let response = await fetch('http://localhost:8080/api/user/register', {
                     method: 'POST',
-                    body: JSON.stringify(this.user),
+                    body: JSON.stringify(userData),
                     headers: { 'Content-Type': 'application/json' }
                 });
                 let data = await response.json();
                 if (data.success) {
+                    const token = data.access_token;
+                    localStorage.setItem('client_token', token); // Исправлено на правильное имя переменной 'token'
+                    this.$emit('authenticated', token);
+                    // Перенаправление пользователя на приветственную страницу
                     this.$router.push('/main/welcome');
                 } else {
-                    alert('Произошла ошибка при регистрации: ' + JSON.stringify(data.errors));
+                    // Добавление ошибок сервера в массив ошибок клиента
+                    this.errorMessages.push('Произошла ошибка при регистрации: ' + JSON.stringify(data.errors));
                 }
             } catch (error) {
                 console.error('Error:', error);
+                this.errorMessages.push('Ошибка соединения с сервером.');
             }
         },
+
         async login() {
             try {
-                let response = await fetch('your_api_endpoint/login', {
+                let response = await fetch('http://localhost:8080/api/user/login', {
                     method: 'POST',
-                    body: JSON.stringify({ email: this.email, password: this.password }),
+                    body: JSON.stringify({ 
+                        client_email: this.email, 
+                        client_password_hash: this.password 
+                    }),
                     headers: { 'Content-Type': 'application/json' }
                 });
+
                 let data = await response.json();
+
                 if (data.success) {
+                    const token = data.access_token;
+                    localStorage.setItem('client_token', token); // Исправлено на правильное имя переменной 'token'
+                    this.$emit('authenticated', token);
                     this.$router.push('/main/welcome');
                 } else {
                     alert('Ошибка входа: ' + data.error);
--- src/components/profile_element.vue
+++ src/components/profile_element.vue
@@ -2,47 +2,184 @@
     <div id="body">
         <div class="profile-card">
             <div class="profile-header">
-                <img src="https://via.placeholder.com/120" alt="Аватар клиента" class="profile-avatar">
-                <h1>Елена Смирнова</h1>
+                <img :src="userData.client_photo" alt="Аватар клиента" class="profile-avatar" @click="uploadTrigger">
+                <h1>{{ userData.client_first_name }} {{ userData.client_last_name }}</h1>
                 <p>Вы наш клиент!<i class="material-icons" id="fav">favorite</i></p>
+                <input type="file" ref="fileInput" id="fileInput" @change="handleFileUpload" style="display: none" />
             </div>
             <div class="profile-content">
                 <h2>Контакты</h2>
-                <p><i class="material-icons" id="email">email</i> elena@mail.com</p>
-                <p><i class="material-icons">phone</i> +7 905 123 4567</p>
-                <p><i class="material-icons">celebration</i>30.10.2005</p>
+                <p><i class="material-icons" id="email">email</i> {{ userData.client_email }}</p>
+                <p><i class="material-icons">phone</i> {{ userData.client_phone }}</p>
+                <p><i class="material-icons">celebration</i>{{ userData.client_birth_date }}</p>
                 <button class="btn" @click="showModal = true">Изменить данные</button>
-                <button class="btn">Мои абонементы</button>
+                <button class="btn" @click="showSubscriptionsModal = true">Мои абонементы</button>
             </div>
             <div v-if="showModal" class="modal">
                 <div class="modal-content">
                     <span class="close" @click="showModal = false">&times;</span>
                     <h2>Изменение данных</h2>
-                    <form>
+                    <form @submit.prevent="updateUserData">
+                        <label for="firstName">Имя:</label>
+                        <input class="prof_input" type="text" id="firstName" name="firstName" v-model="userData.client_first_name"><br>
+                        <label for="lastName">Фамилия:</label>
+                        <input class="prof_input" type="text" id="lastName" name="lastName" v-model="userData.client_last_name"><br>
+                        <label for="patronymic">Отчество:</label>
+                        <input class="prof_input" type="text" id="patronymic" name="patronymic" v-model="userData.client_middle_name"><br>
                         <label for="email">Email:</label>
-                        <input class="prof_input" type="email" id="email" name="email" value="elena@mail.com"><br>
+                        <input class="prof_input" type="email" id="email" name="email" v-model="userData.client_email"><br>
                         <label for="phone">Телефон:</label>
-                        <input class="prof_input" type="text" id="phone" name="phone" value="+7 905 123 4567"><br>
+                        <input class="prof_input" type="text" id="phone" name="phone" v-model="userData.client_phone"><br>
                         <label for="birthdate">Дата рождения:</label>
-                        <input class="prof_input" type="date" id="birthdate" name="birthdate" value="2005-10-30"><br>
+                        <input class="prof_input" type="date" id="birthdate" name="birthdate" v-model="userData.client_birth_date"><br>
                         <button type="submit" class="btn">Сохранить изменения</button>
                     </form>
                 </div>
             </div>
+
+            <div v-if="showSubscriptionsModal" class="modal">
+                <div class="modal-content">
+                    <span class="close" @click="showSubscriptionsModal = false">&times;</span>
+                    <h2>Мои абонементы</h2>
+                    <div v-if="subscriptions.success" id="memberships_list">
+                        <div class="subscription-card" v-for="subscription in subscriptions.data" :key="subscription.membership_id">
+                            <h3>{{ subscription.membership_type_name }}</h3>
+                            <p>Дата покупки: {{ subscription.purchase_date }}</p>
+                            <p>Действителен до: {{ subscription.expiration_date }}</p>
+                            <p>Остаток занятий: {{ subscription.classes_remaining }}</p>
+                            <p v-if="subscription.active_status">Статус: Активен</p>
+                            <p v-else>Статус: Неактивен</p>
+                        </div>
+                    </div>
+                    <p v-else>{{ subscriptions.message }}</p>
+                </div>
+            </div>
+
         </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
     data() {
         return {
-            showModal: false
+            userData: {},
+            subscriptions: [],
+            showModal: false,
+            showSubscriptionsModal: false,
+        };
+    },
+    created() {
+        this.fetchUserInfo();
+    },
+    mounted() {
+        this.fetchSubscriptions();
+    },
+    methods: {
+        async fetchUserInfo() {
+            const token = localStorage.getItem('client_token');
+            try {
+                const response = await fetch('http://localhost:8080/api/user/user-info', {
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                    }
+                }); 
+                const data = await response.json();
+                if (data.success) {
+                    this.userData = data.data;
+                } else {
+                    console.error(data.message);
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        },
+        async updateUserData() {
+            const token = localStorage.getItem('client_token');
+            try {
+                const response = await fetch('http://localhost:8080/api/user/update-profile', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`
+                    },
+                    body: JSON.stringify({
+                        client_first_name: this.userData.client_first_name,
+                        client_last_name: this.userData.client_last_name,
+                        client_middle_name: this.userData.client_middle_name,
+                        client_email: this.userData.client_email,
+                        client_phone: this.userData.client_phone,
+                        client_birth_date: this.userData.client_birth_date
+                    })
+                });
+                const data = await response.json();
+                if (data.success) {
+                    alert('Данные обновлены успешно');
+                } else {
+                    console.error(data.message);
+                    alert('Ошибка: ' + data.message);
+                }
+            } catch (error) {
+                console.error('Произошла ошибка при отправке данных', error);
+                alert('Произошла ошибка при отправке данных');
+            }
+        },
+        uploadTrigger() {
+                this.$refs.fileInput.click();
+        },
+        handleFileUpload(event) {
+            const token = localStorage.getItem('client_token');
+            const file = event.target.files[0];
+            if (!file) return;
+
+            const formData = new FormData();
+            formData.append('avatar', file);
+
+            // Отправка файла на сервер через axios или другой HTTP клиент
+            axios.post('http://localhost:8080/api/user/upload-avatar', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+            .then(response => {
+                if (response.data.success) {
+                    this.userData.client_photo = response.data.url;
+                } else {
+                    console.error(response.data.message);
+                    // Обрабатывайте ошибку по вашему усмотрению
+                }
+            })
+            .catch(error => {
+                console.error('Ошибка при загрузке файла: ', error);
+            });
+        },
+        fetchSubscriptions() {
+            const token = localStorage.getItem('client_token');
+            // Имитация вызова API
+            axios.get('http://localhost:8080/api/membership/list-my-memberships', {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+                .then(response => {
+                        // Проверяем, если сервер вернул успех, сохраняем данные
+                        if (response.data.success) {
+                            this.subscriptions = response.data;
+                        } else {
+                            // Если данных нет, сохраняем сообщение для пользователя
+                            this.subscriptions = { success: false, message: response.data.message };
+                        }
+                    })
+                    .catch(error => {
+                        console.error("Ошибка при получении данных:", error);
+                        this.subscriptions = { success: false, message: 'Произошла ошибка при загрузке данных.' };
+                    });
         }
     }
-}
+};
 </script>
-
 
 <style scoped>
 
@@ -66,6 +203,23 @@ export default {
     color: white;
     height: 90%;
     width: 90%;
+}
+
+#memberships_list {
+    margin-top: 20px;
+    display: flex;
+    justify-content: center;
+}
+
+.subscription-card {
+    border: 1px solid #ccc;
+    padding: 10px;
+    margin: 10px 0;
+    border-radius: 5px;
+}
+
+.subscription-card h3 {
+    margin-top: 0;
 }
 
 
@@ -144,6 +298,8 @@ label {
     border-radius: 10px;
     width: 80%;
     max-width: 600px;
+    min-height: 500px;
+    text-align: center;
 }
 
 .close {
